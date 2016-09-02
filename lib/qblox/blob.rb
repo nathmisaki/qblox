af9b86e4b0b52701ed52bea443be85343e837df2
Nelson Haraguchi <nelsonmhjr@gmail.com>
2016-09-02 19:29:30 -0300

    PErmit public

--- lib/qblox/blob.rb
+++ lib/qblox/blob.rb
@@ -3,9 +3,12 @@ module Qblox
     attr_accessor(:blob_status, :content_type, :created_at, :id,
                   :last_read_access_ts, :lifetime, :name, :public,
                   :ref_count, :set_completed_at, :size, :uid, :updated_at)
-    def self.create(file_path, content_type: nil, name: nil, token: )
+    def self.create(file_path, options={})
+      content_type = options[:content_type]
+      name = options[:name]
+      token = options.delete(:token)
       api = Qblox::Api::Blob.new(token: token)
-      data = api.create(name: name, content_type: content_type)
+      data = api.create(options)
       length = api.upload_file(file_path, data['blob']['blob_object_access'])
       return unless length
       return unless api.complete(data['blob']['id'], length)
